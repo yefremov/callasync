8bd2218891dfbdc19380fe43b4d45b6d98a7d762
Anton Yefremov <anton.yefremov@gmail.com>
2017-02-15 01:44:31 +0200

    Improve performance (#2)

--- index.js
+++ index.js
@@ -5,19 +5,42 @@
 
 module.exports = callAsync;
 
+/**
+ * A queue holding functions to execute on timeout.
+ * @type {Array}
+ */
+
 var waiting = [];
+
+/**
+ * Waiting queue timeout handle.
+ * @type {number}
+ */
+
 var waitingID = 0;
 
+/**
+ * Waiting queue handler function.
+ * @type {Function}
+ */
+
 var callWaiting = function () {
-  var funcs = waiting;
+  var index = -1;
+  var length = waiting.length;
+  var funcs = waiting; // local scope copy of waiting queue
 
-  waiting = [];
-  waitingID = 0;
+  waiting = []; // drop waiting queue
+  waitingID = 0; // reset waiting handle
 
-  while (funcs.length) {
-    funcs.shift()();
+  while (++index < length) {
+    funcs[index]();
   }
 };
+
+/**
+ * Add `func` to the waiting queue to execute it on the next timeout.
+ * @param {Function} func The function to delay.
+ */
 
 function callAsync(func) {
   waiting.push(func);
